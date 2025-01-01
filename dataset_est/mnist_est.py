dbcd41d194b6edf59046bd27476c10c50c4538d1
Senura Hansaja <87333867+SenuraHansaja@users.noreply.github.com>
2025-01-01 18:37:52 +0700

    Update mnist_est.py
    add the kl_div_mnist

--- dataset_est/mnist_est.py
+++ dataset_est/mnist_est.py
@@ -1,5 +1,9 @@
 import numpy as np
 from sklearn.datasets import fetch_openml
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+
 
 # Load MNIST dataset
 def entropy_mnist():
@@ -14,4 +18,52 @@ def entropy_mnist():
     # Calculate entropy
     entropy = -np.sum(probabilities * np.log2(probabilities))
     # print("Entropy: ", entropy)
-    return entropy
+    return entropy   
+
+
+
+def mnist_distribuiton(eta: float):
+    """
+    This function is used for the distribution calculation of the MNIST dataset,
+    eta -  sampling ratio is passed and datasets is split according to that and 
+    then calculate the dataset distribution
+    Args : 
+        - eta - sampling ratio
+    Return:
+        - Probability Distribution of the datasets
+    """
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    all_labels = np.concatenate((train_labels, test_labels))
+    split_val = len(all_labels)*eta
+    split_labels = all_labels[:round(split_val)]    
+    
+    ## counting the how many samples are there in each class 
+    class_counts = np.bincount(split_labels)
+    
+    total_sample = len(split_labels)
+    class_distribution = class_counts / total_sample
+    
+    return class_distribution   ## this gives an array 
+    
+    
+
+def kl_div_mnist(eta: float):
+    """
+    This used to calculate the KL divergence between the distributions of the sampled data 
+    and the original datasets
+    Args:
+        - eta - sampling ratio
+    Return 
+        - KL-Div beteween the sampled data and the original dataset
+    """
+    (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+    all_labels = np.concatenate((train_labels, test_labels))
+    
+    class_counts = np.bincount(all_labels)
+    
+    total_sample = len(all_labels)
+    class_distribution = class_counts / total_sample
+    
+    kl = np.sum(mnist_distribuiton(eta) * np.log(mnist_distribuiton(eta) / class_distribution)) 
+    
+    return kl
